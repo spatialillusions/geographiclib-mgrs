3119d67f35e5e4f4cb2973e76c5a242482c1d412
Måns Beckman <mans.beckman@spatialillusions.com>
2025-03-14 13:52:55 +0100

    Update geotrans-mgrsToGeo.test.js

--- test/geotrans-mgrsToGeo.test.js
+++ test/geotrans-mgrsToGeo.test.js
@@ -19,23 +19,56 @@ for (let i = 0; i < geotrans.length; i++) {
 const WE = {};
 let mgrs, point, lat, lon, latRef, lonRef;
 
-const i = 300;
+const i = 400;
 mgrs = geotrans[i][7].replace(" ", "");
 point = MGRS.toPoint(mgrs, true);
 lat = point[1].toFixed(6);
 lon = point[0].toFixed(6);
-latRef = geotrans[i][10];
-lonRef = geotrans[i][11];
 
+//console.log(geotrans[0][10 + 3] + " " + geotrans[1][10 + 3]);
+//console.log(geotrans[0][11 + 3] + " " + geotrans[1][11 + 3]);
+latRef = geotrans[i][13];
+lonRef = geotrans[i][14];
+
+/*
 WE[mgrs] = [
   { lat: lat, lon: lon },
   { lat: latRef, lon: lonRef },
 ];
+*/
 
-WE[mgrs + " geographiclib"] = [
+// .\GeoConvert_d.exe  -p 1 --input-string "33XVK9556495053"
+// 81.016479 14.745563
+point = MGRS.toPoint("33XVK9556495053", true);
+lat = point[1].toFixed(6);
+lon = point[0].toFixed(6);
+
+WE["33XVK9556495053" + " geographiclib"] = [
   { lat: lat, lon: lon },
-  { lat: "9.042052", lon: "7.180482" },
+  { lat: "81.016479", lon: "14.745563" },
 ];
 
-// 9.042052 7.180482
+// Geoconvert outputs the expected MGRS
+// .\GeoConvert_d.exe -m -p 0 --input-string "81.016475 14.745534"
+// 33XVK9556395053
+
+// .\GeoConvert_d.exe  -p 1 --input-string "31PGK5976098847"
+// 9.028532 5.362851
+point = MGRS.toPoint("31PGK5976098847", true);
+lat = point[1].toFixed(6);
+lon = point[0].toFixed(6);
+
+WE["31PGK5976098847" + " geographiclib"] = [
+  { lat: lat, lon: lon },
+  { lat: "9.028532", lon: "5.362851" },
+];
+
+/*
+// 9.028532 5.362851
+WE[mgrs + " geographiclib"] = [
+  { lat: lat, lon: lon },
+  { lat: "9.028532", lon: "5.362851" },
+];
+*/
+
 export default { "Geotrans MGRS to Geo WE": WE };
