45748bdc66ac27f9edeb09a7d1cdd5cb1e59b9d0
Måns Beckman <mans.beckman@spatialillusions.com>
2025-03-16 11:18:16 +0100

    Create to-geo-and-back.test.js

---
+++ test/to-geo-and-back.test.js
@@ -0,0 +1,13 @@
+import MGRS from "../index.mjs";
+
+/* Testing that we don't get a different MGRS back when we go back and fourth */
+const mgrs = "42SUF1230045600";
+const centerpoint = false;
+const point = MGRS.toPoint(mgrs, centerpoint);
+
+const accuracy = 5;
+const result = MGRS.forward(point, accuracy);
+
+export default {
+  "MGRS to point and back": [mgrs, result],
+};
